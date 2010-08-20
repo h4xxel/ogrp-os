43e76375c5c33b0ffac640a66042190a165b835c
mr3d <h4xxel@h4xx.org>
2010-08-20 17:56:15 +0200

    uppdaterade datum, mm

--- src/boot/stage1/boot.asm
+++ src/boot/stage1/boot.asm
@@ -1,5 +1,5 @@
 ; * This file is a part of the ogrp-os project
-; * Version: 0.1 19 Aug 2010
+; * Version: 0.1 20 Aug 2010
 ;
 ; * Authors: jockepockee, mr3d (jockepockee.com, h4xxel.ath.cx)
 ; * Email: jocke@h4xx.org h4xxel@h4xx.org
--- src/boot/stage2/gdt.asm
+++ src/boot/stage2/gdt.asm
@@ -1,3 +1,12 @@
+; * This file is a part of the ogrp-os project
+; * Version: 0.1 20 Aug 2010
+;
+; * Authors: jockepockee, mr3d (jockepockee.com, h4xxel.ath.cx)
+; * Email: jocke@h4xx.org h4xxel@h4xx.org
+;
+; * Copyright 2010 ogrp
+; * License: see COPYING file	
+
 LoadGDT:
 	push	cs
 	pop	ds
